3e2a4b2b6219f58c17ba8a0f69a4d6fce565b9bd
sigmarion1 <axsim@naver.com>
2022-05-27 17:04:39 +0900

    disable sequelize

--- back/app.js
+++ back/app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
-const session = require('express-session')
+// const session = require('express-session')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,37 +8,37 @@ var logger = require('morgan');
 const dotenv = require('dotenv')
 dotenv.config({ path: path.join(__dirname, "..", ".env" )})
 
-const passport = require('passport')
+// const passport = require('passport')
 
-const apiRouter = require('./routes/api')
-const { sequelize } = require('./models');
-const passportConfig = require('./passport')
+// const apiRouter = require('./routes/api')
+// const { sequelize } = require('./models');
+// const passportConfig = require('./passport')
 
 var app = express();
-sequelize.sync();
-passportConfig(passport)
+// sequelize.sync();
+// passportConfig(passport)
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  resave: true,
-  saveUninitialized: false,
-  secret: 'secret',
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(session({
+//   resave: true,
+//   saveUninitialized: false,
+//   secret: 'secret',
+//   cookie: {
+//     httpOnly: true,
+//     secure: false,
+//   }
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 
 app.use(express.static("build"))
 
-app.use('/api', apiRouter)
+// app.use('/api', apiRouter)
 
 app.get('*', function (request, response){
   response.sendFile(path.join(__dirname, 'build', 'index.html'))
