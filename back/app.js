f8122f0deb7562cdf527b9686de979258d574e78
sigmarion1 <axsim@naver.com>
2022-09-09 21:11:51 +0900

    change setup for inginx

--- back/app.js
+++ back/app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
+var createError = require("http-errors");
+var express = require("express");
 // const session = require('express-session')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const dotenv = require('dotenv')
-dotenv.config({ path: path.join(__dirname, "..", ".env" )})
+// const dotenv = require("dotenv");
+// dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 // const passport = require('passport')
 
@@ -18,11 +18,11 @@ var app = express();
 // sequelize.sync();
 // passportConfig(passport)
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 // app.use(session({
 //   resave: true,
 //   saveUninitialized: false,
@@ -35,22 +35,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(passport.initialize())
 // app.use(passport.session())
 
-
-app.use(express.static("build"))
+app.use(express.static("build"));
 
 // app.use('/api', apiRouter)
 
-app.get('*', function (request, response){
-  response.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.get("*", function (request, response) {
+  response.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   // res.locals.message = err.message;
   // res.locals.error = req.app.get('env') === 'development' ? err : {};
